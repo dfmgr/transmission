7fe9901403283f57341244cee71b4e873a1fb470
Jason <jason@casjay.net>
2021-02-09 02:18:22 -0500

    🦈🏠❗ Well I did things - Hopefully nothing breaks ❗🏠🦈

--- install.sh
+++ install.sh
@@ -1,223 +1,180 @@
 #!/usr/bin/env bash
-
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 APPNAME="transmission"
 USER="${SUDO_USER:-${USER}}"
 HOME="${USER_HOME:-${HOME}}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#set opts
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# @Author          : Jason
-# @Contact         : casjaysdev@casjay.net
-# @File            : install.sh
-# @Created         : Fr, Aug 28, 2020, 00:00 EST
-# @License         : WTFPL
-# @Copyright       : Copyright (c) CasjaysDev
-# @Description     : installer script for transmission
-#
+##@Version       : 020820212259-git
+# @Author        : Jason Hempstead
+# @Contact       : jason@casjaysdev.com
+# @License       : LICENSE.md
+# @ReadME        : README.md
+# @Copyright     : Copyright: (c) 2021 Jason Hempstead, CasjaysDev
+# @Created       : Monday, Feb 08, 2021 22:59 EST
+# @File          : transmission
+# @Description   : Installer script for transmission
+# @TODO          :
+# @Other         :
+# @Resource      :
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# Set functions
-
-SCRIPTSFUNCTURL="${SCRIPTSAPPFUNCTURL:-https://github.com/casjay-dotfiles/scripts/raw/master/functions}"
-SCRIPTSFUNCTDIR="${SCRIPTSAPPFUNCTDIR:-/usr/local/share/CasjaysDev/scripts}"
+# Import functions
+CASJAYSDEVDIR="${CASJAYSDEVDIR:-/usr/local/share/CasjaysDev/scripts}"
+SCRIPTSFUNCTDIR="${CASJAYSDEVDIR:-/usr/local/share/CasjaysDev/scripts}/functions"
 SCRIPTSFUNCTFILE="${SCRIPTSAPPFUNCTFILE:-app-installer.bash}"
-
+SCRIPTSFUNCTURL="${SCRIPTSAPPFUNCTURL:-https://github.com/dfmgr/installer/raw/master/functions}"
+connect_test() { ping -c1 1.1.1.1 &>/dev/null || curl --disable -LSs --connect-timeout 3 --retry 0 --max-time 1 1.1.1.1 2>/dev/null | grep -e "HTTP/[0123456789]" | grep -q "200" -n1 &>/dev/null; }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-if [ -f "$SCRIPTSFUNCTDIR/functions/$SCRIPTSFUNCTFILE" ]; then
-  . "$SCRIPTSFUNCTDIR/functions/$SCRIPTSFUNCTFILE"
-elif [ -f "$HOME/.local/share/CasjaysDev/functions/$SCRIPTSFUNCTFILE" ]; then
-  . "$HOME/.local/share/CasjaysDev/functions/$SCRIPTSFUNCTFILE"
-else
+if [ -f "$PWD/$SCRIPTSFUNCTFILE" ]; then
+  . "$PWD/$SCRIPTSFUNCTFILE"
+elif [ -f "$SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE" ]; then
+  . "$SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE"
+elif connect_test; then
   curl -LSs "$SCRIPTSFUNCTURL/$SCRIPTSFUNCTFILE" -o "/tmp/$SCRIPTSFUNCTFILE" || exit 1
   . "/tmp/$SCRIPTSFUNCTFILE"
+else
+  echo "Can not load the functions file: $SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE" 1>&2
+  exit 1
 fi
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Call the main function
 user_installdirs
-
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # OS Support: supported_os unsupported_oses
-
 unsupported_oses
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 # Make sure the scripts repo is installed
-
 scripts_check
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 # Defaults
 APPNAME="${APPNAME:-transmission}"
 APPDIR="${APPDIR:-$HOME/.config/$APPNAME}"
 INSTDIR="${INSTDIR}"
-REPO="${DFMGRREPO:-https://github.com/dfmgr}/${APPNAME}"
+REPO="${DFMGRREPO:-https://github.com/dfmgr/$APPNAME}"
 REPORAW="${REPORAW:-$REPO/raw}"
-APPVERSION="$(__appversion)"
-
+APPVERSION="$(__appversion "$REPORAW/master/version.txt")"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 # Setup plugins
-
 PLUGNAMES=""
 PLUGDIR="${SHARE:-$HOME/.local/share}/$APPNAME"
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# dfmgr_install fontmgr_install iconmgr_install pkmgr_install systemmgr_install thememgr_install wallpapermgr_install
-
+# Call the dfmgr function
 dfmgr_install
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# Version
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# Script options IE: --help
-
+# Script options IE: --help --version
 show_optvars "$@"
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 # Do not update
-
-#systemmgr_noupdate
-
+#installer_noupdate
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 # Requires root - no point in continuing
-
 #sudoreq  # sudo required
 #sudorun  # sudo optional
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 # end with a space
-
-APP="$APPNAME-cli transmission-daemon "
+APP="$APPNAME transmission-cli transmission-daemon"
 PERL=""
 PYTH=""
 PIPS=""
 CPAN=""
 GEMS=""
-
-# install packages - useful for package that have the same name on all oses
-install_packages $APP
-
-# install required packages using file
-install_required $APP
-
-# check for perl modules and install using system package manager
-install_perl $PERL
-
-# check for python modules and install using system package manager
-install_python $PYTH
-
-# check for pip binaries and install using python package manager
-install_pip $PIPS
-
-# check for cpan binaries and install using perl package manager
-install_cpan $CPAN
-
-# check for ruby binaries and install using ruby package manager
-install_gem $GEMS
-
-# Other dependencies
-dotfilesreq git
-dotfilesreqadmin
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
+# install packages - useful for package that have the same name on all oses
+install_packages "$APP"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# install required packages using file
+install_required "$APP"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# check for perl modules and install using system package manager
+install_perl "$PERL"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# check for python modules and install using system package manager
+install_python "$PYTH"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# check for pip binaries and install using python package manager
+install_pip "$PIPS"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# check for cpan binaries and install using perl package manager
+install_cpan "$CPAN"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# check for ruby binaries and install using ruby package manager
+install_gem "$GEMS"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Other dependencies
+dotfilesreq git misc
+dotfilesreqadmin
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Ensure directories exist
-
 ensure_dirs
 ensure_perms
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# Main progam
-
-devnull kill -9 "$(pidof transmission-gt)"
-devnull kill -9 "$(pidof transmission-daemon)"
-devnull kill -9 "$(pidof transmission-remote-gtk)"
-
+# Backup if needed
 if [ -d "$APPDIR" ]; then
   execute "backupapp $APPDIR $APPNAME" "Backing up $APPDIR"
 fi
-
-if [ -d "$INSTDIR/.git" ]; then
-  execute \
-    "git_update $INSTDIR" \
-    "Updating $APPNAME configurations"
-else
-  execute \
-    "git_clone $REPO/$APPNAME $INSTDIR" \
-    "Installing $APPNAME configurations"
+# Main progam
+devnull kill -9 "$(pidof transmission-gt)"
+devnull kill -9 "$(pidof transmission-daemon)"
+devnull kill -9 "$(pidof transmission-remote-gtk)"
+if __am_i_online; then
+  if [ -d "$INSTDIR/.git" ]; then
+    execute "git_update $INSTDIR" "Updating $APPNAME configurations"
+  else
+    execute "git_clone $REPO/$APPNAME $INSTDIR" "Installing $APPNAME configurations"
+  fi
+  # exit on fail
+  failexitcode $? "Git has failed"
 fi
-
-# exit on fail
-failexitcode
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 # Plugins
-
 if __am_i_online; then
   if [ "$PLUGNAMES" != "" ]; then
     if [ -d "$PLUGDIR"/PLUREP/.git ]; then
-      execute \
-        "git_update $PLUGDIR/PLUGREP" \
-        "Updating plugin PLUGNAME"
+      execute "git_update $PLUGDIR/PLUGREP" "Updating plugin PLUGNAME"
     else
-      execute \
-        "git_clone PLUGINREPO $PLUGDIR/PLUGREP" \
-        "Installing plugin PLUGREP"
+      execute
+      "git_clone PLUGINREPO $PLUGDIR/PLUGREP" "Installing plugin PLUGREP"
     fi
   fi
-
   # exit on fail
-  failexitcode
+  failexitcode $? "Git has failed"
 fi
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 # run post install scripts
-
 run_postinst() {
   dfmgr_run_post
-  mkd "$HOME/Downloads" "$HOME/.local/share/torrents/Complete" "$HOME/.local/share/torrents/InComplete"
+  mkd "$HOME/Downloads" "$HOME/Downloads/Complete" "$HOME/Downloads/InComplete"
+  mkd "$HOME/.config/transmission-remote-gtk" "$HOME/.config/transmission-daemon"
   if [ -n "$TRANSMISSION_SERVER" ]; then
     replace "$APPDIR/transmission-remote-gtk.json" "transmission_server" "$TRANSMISSION_SERVER"
   else replace "$APPDIR/transmission-remote-gtk.json" "transmission_server" "localhost"; fi
   replace "$APPDIR/settings.json" "replacehome" "$HOME"
-  ln_sf "$HOME/.local/share/torrents" "$HOME/Downloads/Torrents"
-  ln_sf "$APPDIR" "$HOME/.config/transmission-remote-gtk"
-  ln_sf "$APPDIR" "$HOME/.config/transmission-daemon"
+  ln_sf "$HOME/Downloads/Torrents" "$HOME/.local/share/torrents"
   ln_sf "$APPDIR/settings.json" "$HOME/.config/transmission-daemon/settings.json"
   ln_sf "$APPDIR/transmission-remote-gtk.json" "$HOME/.config/transmission-remote-gtk/config.json"
   #system_service_disable --now transmission-daemon.service
   if cmd_exists transmission-daemon; then
     transmission-daemon &
-  fi
-  if cmd_exist transmission-remote-gtk; then
-    transmission-remote-gtk -m &
+    disown
+    cmd_exists transmission-remote-gtk && transmission-remote-gtk -m &>/dev/null &
+    disown
+  elif [ -n "$DESKTOP_SESSION" ] && netstatg transmission | grep -q 9091; then
+    mybrowser http://localhost:9091 &
+  elif cmd_exists transmission-gtk; then
+    if [ -n "$DESKTOP_SESSION" ]; then
+      transmission-gtk -m &>/dev/null &
+      disown
+    fi
   fi
 }
-
-execute \
-  "run_postinst" \
-  "Running post install scripts"
-
+#
+execute "run_postinst" "Running post install scripts"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 # create version file
-
 dfmgr_install_version
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 # exit
 run_exit
-
 # end
